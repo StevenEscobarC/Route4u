bb8720e71269c0e4f73c6997bebf66bebdcaef46
StevenEscobarC <stevenlikeplis@gmail.com>
2022-04-08 18:23:22 -0500

    Item de desarrollo 1, CRUD tipo de vehiculo y conexión base de datos

---
+++ app/src/main/java/com/example/route4you/model/TipoVehiculo.java
@@ -0,0 +1,40 @@
+package com.example.route4you.model;
+
+public class TipoVehiculo {
+
+    private String uid;
+    private String name;
+    private String year;
+
+    public TipoVehiculo() {
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
