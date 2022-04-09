adf97514cc90a55ff9f0cc4a9368dddab66ad20b
danydlpr <danieldelapavar@gmail.com>
2022-04-09 16:03:00 -0500

    Item 2

--- app/src/main/java/com/example/route4you/MainActivity.java
+++ app/src/main/java/com/example/route4you/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.route4you;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button btnGasolina=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+        btnGasolina=findViewById(R.id.buttonTipoC);
+        btnGasolina.setOnClickListener(View -> onClick());
     }
+    public void onClick(){
+        Intent miIntent = new Intent(MainActivity.this,GasolinaActivity.class);
+        startActivity(miIntent);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/route4you/Model/TipoGasolina.java
@@ -0,0 +1,31 @@
+package com.example.route4you.Model;
+
+public class TipoGasolina {
+
+    private String uid;
+    private String name;
+
+    public TipoGasolina() {
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
